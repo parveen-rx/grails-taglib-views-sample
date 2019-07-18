812a5cc0dae2ed449e4124305939e3179589a0c0
parveen-rx <parveen.soni@rxlogix.com>
2019-07-18 14:59:35 +0530

    update create user sql file
    update create user sql file

--- data/create-user.sql
+++ data/create-user.sql
@@ -1,6 +1,13 @@
---run below sql statement from a user in the DB for application
+--run below sql statement from a sys user to create a user in the DB for application
 
---run as many as you want after app start
-INSERT INTO STUDENT(age, city, country, deleted, gender, name) VALUES (27, 'Noida', 'India', 0, 'Male', 'Parveen Soni');
+CREATE TABLESPACE tbs_gsa DATAFILE 'tbs_gsa.DBF' SIZE 10M AUTOEXTEND ON;
+
+CREATE TABLESPACE tbs_lob_gsa DATAFILE 'tabs_lob_gsa.DBF' SIZE 10M AUTOEXTEND ON;
+
+CREATE USER gsdbu identified BY gsdbu default tablespace tbs_gsa quota unlimited on tbs_gsa QUOTA UNLIMITED ON tbs_lob_gsa;
+
+Commit;
+
+grant dba to gsdbu;
 
 Commit;
